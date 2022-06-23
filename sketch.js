647841cf7de3cf9fe933ec51fad6ddf11a60539f
GuilhermeCanedo <108073951+GuilhermeCanedo@users.noreply.github.com>
2022-06-23 10:21:50 -0300

    V2

--- sketch.js
+++ sketch.js
@@ -9,8 +9,17 @@ function draw()
 {
   background(30);
   if (keyDown("right")) {
- quadrado.x= quadrado.x+ 2   
+ quadrado.x= quadrado.x+ 2 
   }
+  if (keyDown("left")) {
+    quadrado.x= quadrado.x-2 
+     } 
+     if (keyDown("up")) {
+      quadrado.y= quadrado.y-2 
+       }
+       if (keyDown("down")) {
+        quadrado.y= quadrado.y+ 2
+         }
 drawSprites()
 }
 
